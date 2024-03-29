6cac79dcb4e6ca7cc6ecd6986174c0d6be6329e1
Yeasir Arafat <yeasirarafat321@gmail.com>
2024-03-30 03:37:24 +0600

    logo updated

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,3 +1,5 @@
 <div class="text-gray-800 font-semibold">
-    <span class="text-yellow-500 text-xl">Hridoy's</span>
+    <a href="{{ route('home') }}" class="flex items-center space-x-2">
+        <img src="{{ asset('logo.png') }}" alt="Logo" class="w-auto h-8">
+    </a>
 </div>
