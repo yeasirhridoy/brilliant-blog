3978145c87df0409ac6732aad98cd63c7081aed1
Yeasir Arafat <yeasirarafat321@gmail.com>
2024-03-30 17:20:58 +0600

    image size updated

--- resources/views/components/posts/post-card.blade.php
+++ resources/views/components/posts/post-card.blade.php
@@ -3,7 +3,7 @@
 <div {{ $attributes }}>
     <a wire:navigate href="{{ route('posts.show', $post->slug) }}">
         <div>
-            <img class="w-full rounded-xl" src="{{ $post->getThumbnailUrl() }}">
+            <img class="w-full md:h-40 lg:h-80 object-cover rounded-xl" src="{{ $post->getThumbnailUrl() }}">
         </div>
     </a>
     <div class="mt-3">
