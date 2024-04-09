c1976cdd6dfe3c0dd5ddf7c11fc84e0a393c8c69
Yeasir Arafat <yeasirarafat321@gmail.com>
2024-04-09 23:24:01 +0600

    privacy and terms page updated

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -1,5 +1,50 @@
-<x-app-layout>
+<x-app-layout title="Privacy Policy">
     <div class="pt-4 min-h-screen max-w-4xl mx-auto">
-        Privacy Policy Page
+        <article>
+            <h1 class="text-4xl">Privacy Policy for Hridoy's Blog Website</h1>
+
+            <section>
+                <h2 class="text-2xl mt-4">Introduction</h2>
+                <p>At Hridoy's, we take your privacy seriously. This privacy policy explains how we collect, use, and protect your personal information when you use our website.</p>
+            </section>
+
+            <section>
+                <h2 class="text-2xl mt-4">Information We Collect</h2>
+                <p>We may collect personal information such as your name and email address when you voluntarily submit it to us through forms on our website. Additionally, we may collect non-personal information such as browser type, operating system, and pages visited to improve our website's performance.</p>
+            </section>
+
+            <section>
+                <h2 class="text-2xl mt-4">How We Use Your Information</h2>
+                <p>We may use the information we collect to:</p>
+                <ul>
+                    <li>Respond to your inquiries or requests</li>
+                    <li>Send you newsletters, updates, or marketing communications</li>
+                    <li>Improve our website and services</li>
+                    <li>Analyze user trends and preferences</li>
+                    <li>Comply with legal obligations</li>
+                </ul>
+            </section>
+
+            <section>
+                <h2 class="text-2xl mt-4">Information Sharing</h2>
+                <p>We do not sell, trade, or otherwise transfer your personal information to outside parties unless we provide you with advance notice or obtain your consent. However, we may share your information with trusted third parties who assist us in operating our website, conducting our business, or servicing you, as long as those parties agree to keep this information confidential.</p>
+            </section>
+
+            <section>
+                <h2 class="text-2xl mt-4">Security</h2>
+                <p>We implement security measures to protect your personal information from unauthorized access, alteration, disclosure, or destruction. However, please be aware that no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.</p>
+            </section>
+
+            <section>
+                <h2 class="text-2xl mt-4">Changes to This Policy</h2>
+                <p>We may update this privacy policy from time to time by posting the revised version on this page. We encourage you to review this page periodically for any changes. Your continued use of our website after the posting of changes constitutes your acceptance of such changes.</p>
+            </section>
+
+            <section>
+                <h2 class="text-2xl mt-4">Contact Us</h2>
+                <p>If you have any questions or concerns about our privacy policy, please contact us at info@hridoys.com</p>
+            </section>
+
+        </article>
     </div>
 </x-app-layout>
--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,5 +1,108 @@
-<x-app-layout>
+<x-app-layout title="Terms of Service">
     <div class="pt-4 min-h-screen max-w-4xl mx-auto">
-        Terms and Conditions Page
+        <article>
+            <h1 class="text-4xl">Terms and Conditions for Hridoy's Blog Website</h1>
+            <section>
+                <h2 class="text-2xl mt-4">Introduction</h2>
+                <p>
+                    Welcome to Hridoy's blog website. These terms and conditions govern your use of this website. By
+                    accessing or using this website, you agree to abide by these terms and conditions in full. If you
+                    disagree with any part of these terms and conditions, please refrain from using this website.
+                </p>
+            </section>
+
+            <section>
+                <h2 class="text-2xl mt-4">Intellectual Property Rights</h2>
+                <p>
+                    Unless otherwise stated, Hridoy's and/or its licensors own the intellectual property rights for all
+                    material on this website. All intellectual property rights are reserved. You may view and/or print
+                    pages
+                    from this website for your own personal use subject to restrictions set in these terms and
+                    conditions.
+                </p>
+            </section>
+
+            <section>
+                <h2 class="text-2xl mt-4">User Content</h2>
+                <p>
+                    By posting or submitting content to Hridoy's blog website, you grant us a non-exclusive,
+                    royalty-free,
+                    perpetual, irrevocable, and fully sublicensable right to use, reproduce, modify, adapt, publish,
+                    translate, create derivative works from, distribute, and display such content throughout the world
+                    in
+                    any media.
+                    You represent and warrant that you own or otherwise control all of the rights to the content that
+                    you
+                    post, that the content is accurate, that use of the content you supply does not violate this policy
+                    and
+                    will not cause injury to any person or entity, and that you will indemnify Hridoy's for all claims
+                    resulting from content you supply.
+                </p>
+            </section>
+            <section>
+                <h2 class="text-2xl mt-4">Limitations of Liability</h2>
+                <p>
+                    Hridoy's will not be liable to you (whether under the law of contract, the law of torts or
+                    otherwise)
+                    in relation to the contents of, or use of, or otherwise in connection with, this website:
+                </p>
+                <ul class="list-disc ml-5">
+                    <li>
+                        To the extent that the website is provided free-of-charge, for any direct loss;
+                    </li>
+                    <li>
+                        For any indirect, special, or consequential loss; or
+                    </li>
+                    <li>
+                        For any business losses, loss of revenue, income, profits, or anticipated savings, loss of
+                        contracts
+                        or business relationships, loss of reputation or goodwill, or loss or corruption of information
+                        or
+                        data.
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <h2 class="text-2xl mt-4">Indemnity</h2>
+                <p>
+                    You hereby indemnify Hridoy's and undertake to keep Hridoy's indemnified against any losses,
+                    damages, costs, liabilities, and expenses (including without limitation legal expenses and any
+                    amounts paid by Hridoy's to a third party in settlement of a claim or dispute on the advice of
+                    Hridoy's legal advisers) incurred or suffered by Hridoy's arising out of any breach by you of any
+                    provision of these terms and conditions.
+                </p>
+            </section>
+            <section>
+                <h2 class="text-2xl mt-4">Variation</h2>
+                <p>
+                    Hridoy's may revise these terms and conditions from time-to-time. Revised terms and conditions will
+                    apply to the use of this website from the date of the publication of the revised terms and
+                    conditions on this website. Please check this page regularly to ensure you are familiar with the
+                    current version.
+                </p>
+            </section>
+            <section>
+                <h2 class="text-2xl mt-4">Entire Agreement</h2>
+                <p>
+                    These terms and conditions constitute the entire agreement between you and Hridoy's in relation to
+                    your use of this website and supersede all previous agreements in respect of your use of this
+                    website.
+                </p>
+            </section>
+            <section>
+                <h2 class="text-2xl mt-4">Law and Jurisdiction</h2>
+                <p>
+                    These terms and conditions will be governed by and construed in accordance with the laws of the
+                    State of Bangladesh, and any disputes relating to these terms and conditions will be subject to the
+                    exclusive jurisdiction of the courts of the State of Bangladesh.
+                </p>
+            </section>
+            <section>
+                <p class="mt-4">
+                    By using Hridoy's blog website, you agree to these terms and conditions. If you do not agree to
+                    these terms and conditions, please do not use this website.
+                </p>
+            </section>
+        </article>
     </div>
 </x-app-layout>
