e7cd9178c8e296bbe0ab46b524723369be8f7a12
Yeasir Arafat <yeasirarafat321@gmail.com>
2024-12-08 17:12:13 +0600

    updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-adsense-account" content="ca-pub-3624478285703982">
 
     <title> {{ isset($title) ? $title . ' - ' : '' }}{{ config('app.name', '') }}</title>
 
